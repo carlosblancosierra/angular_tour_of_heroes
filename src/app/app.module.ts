308b2154bd6d5e3cf506b28861d1dceeb357b353
Carlos E. Blanco Sierra <carlosblanco@Carloss-MacBook-Pro.local>
2018-01-10 16:41:00 -0400

    3. The Hero Editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { FormsModule } from '@angular/forms' //<-- NgModel Lives here
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
